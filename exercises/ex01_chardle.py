92e730b532e612162d5cfc21cfb5a8a5d01ae6d3
Abhishri Agrawal <abhishri@unc.edu>
2023-01-30 22:52:53 -0500

    Progress on EX01

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,32 @@
+"""EX01 - Chardle - A cute step toward Wordle."""
+
+__author__="730554082"
+
+counter = 0
+fiveCharacterWord: str = input("Enter a 5-character word: ")
+
+if len(fiveCharacterWord) < 5:
+    print("Error: Word must contain 5 characters")
+    exit()
+
+singleCharacter: str = input("Enter a single character: ")
+print("Searching for " + singleCharacter + " in " + fiveCharacterWord)
+
+if fiveCharacterWord[0] == singleCharacter:
+    print(singleCharacter + " found at index 0")
+    counter = counter+1
+if fiveCharacterWord[1] == singleCharacter:
+    print(singleCharacter + " found at index 1")
+    counter = counter+1
+if fiveCharacterWord[2] == singleCharacter:
+    print(singleCharacter + " found at index 2")
+    counter = counter+1
+if fiveCharacterWord[3] == singleCharacter:
+    print(singleCharacter + " found at index 3")
+    counter = counter+1
+if fiveCharacterWord[4] == singleCharacter:
+    print(singleCharacter + " found at index 4")
+    counter = counter+1
+
+print(str(counter) + " instances of " + singleCharacter +  " found in " + fiveCharacterWord)
+
